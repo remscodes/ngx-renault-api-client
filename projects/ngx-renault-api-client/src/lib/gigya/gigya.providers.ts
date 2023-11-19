4112bf9bf391964e4800d6dfb8acd65b8fdca8c7
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-19 22:33:47 +0100

    Move clients into their respective provider

--- projects/ngx-renault-api-client/src/lib/gigya/gigya.providers.ts
+++ projects/ngx-renault-api-client/src/lib/gigya/gigya.providers.ts
@@ -1,9 +1,11 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { EnvironmentProviders, Provider } from '@angular/core';
 import { gigyaInterceptor } from './interceptors/gigya.interceptor';
+import { NgxGigyaClient } from './ngx-gigya-client';
 
 export const GIGYA_PROVIDERS: (Provider | EnvironmentProviders)[] = [
   provideHttpClient(
     withInterceptors([gigyaInterceptor()]),
   ),
+  NgxGigyaClient
 ];
