ffae331bbcd7e19c8cccb3f1df3398919e71466a
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-20 00:37:45 +0100

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './projects/ngx-renault-api-client-demo/src/app/**/*.{html,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
