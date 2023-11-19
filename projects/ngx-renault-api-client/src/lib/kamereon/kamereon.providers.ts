0dd02a1a376e5d45237836c9d861d5d3ce21a600
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-19 22:33:47 +0100

    Move clients into their respective provider

--- projects/ngx-renault-api-client/src/lib/kamereon/kamereon.providers.ts
+++ projects/ngx-renault-api-client/src/lib/kamereon/kamereon.providers.ts
@@ -1,9 +1,11 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { EnvironmentProviders, Provider } from '@angular/core';
 import { kamereonInterceptor } from './interceptors/kamereon.interceptor';
+import { NgxKamereonClient } from './ngx-kamereon-client';
 
 export const KAMEREON_PROVIDERS: (Provider | EnvironmentProviders)[] = [
   provideHttpClient(
     withInterceptors([kamereonInterceptor()]),
   ),
+  NgxKamereonClient
 ];
--- projects/ngx-renault-api-client/src/lib/ngx-renault-client.provider.ts
+++ projects/ngx-renault-api-client/src/lib/ngx-renault-client.provider.ts
@@ -1,8 +1,6 @@
 import { EnvironmentProviders, makeEnvironmentProviders } from '@angular/core';
 import { GIGYA_PROVIDERS } from './gigya/gigya.providers';
-import { NgxGigyaClient } from './gigya/ngx-gigya-client';
 import { KAMEREON_PROVIDERS } from './kamereon/kamereon.providers';
-import { NgxKamereonClient } from './kamereon/ngx-kamereon-client';
 import { NgxRenaultClient } from './ngx-renault-client.service';
 import { NgxRenaultSession } from './ngx-renault-session.service';
 
@@ -11,8 +9,6 @@ export function provideRenaultClient(): EnvironmentProviders {
     GIGYA_PROVIDERS,
     KAMEREON_PROVIDERS,
     NgxRenaultClient,
-    NgxGigyaClient,
-    NgxKamereonClient,
     NgxRenaultSession,
   ]);
 }
