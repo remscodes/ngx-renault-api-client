f953bed2b205d6a84ddcdcd65938d1556dba5588
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-23 23:00:32 +0100

    Add JSDocs

--- projects/ngx-renault-api-client/src/lib/ngx-renault-client.provider.ts
+++ projects/ngx-renault-api-client/src/lib/ngx-renault-client.provider.ts
@@ -4,6 +4,9 @@ import { KAMEREON_PROVIDERS } from './kamereon/kamereon.providers';
 import { NgxRenaultClient } from './ngx-renault-client.service';
 import { NgxRenaultSession } from './ngx-renault-session.service';
 
+/**
+ * Configures Angular Renault Client to be available for injection.
+ */
 export function provideRenaultClient(): EnvironmentProviders {
   return makeEnvironmentProviders([
     GIGYA_PROVIDERS,
