ad435b3a01bef3105433dd390811d7ec1811992c
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-12-06 18:37:32 +0100

    Refactor KamereonClient

--- projects/ngx-renault-api-client/src/lib/kamereon/models/kamereon-url.models.ts
+++ projects/ngx-renault-api-client/src/lib/kamereon/models/kamereon-url.models.ts
@@ -1,8 +1,32 @@
+import { HttpParams } from '@angular/common/http';
 import { KamereonApi } from '@remscodes/renault-api';
-import { Prefixed } from '../../models/shared.model';
+import { MethodsOf, Optional, PrefixWith } from '../../models/shared.model';
+import { NgxKamereonClient } from '../ngx-kamereon-client';
 
 /** @internal **/
-export type ReadApiUrl = keyof Omit<typeof KamereonApi, 'KEY' | Prefixed<'PERFORM_'>>
+export interface ReadArgs extends CommonArgs {
+  apiUrl: ReadApiUrl;
+  params?: HttpParams;
+}
 
 /** @internal **/
-export type PerformApiUrl = keyof Pick<typeof KamereonApi, Extract<keyof typeof KamereonApi, Prefixed<'PERFORM_'>>>
+export interface PerformArgs extends CommonArgs {
+  apiUrl: PerformApiUrl;
+  data: any;
+}
+
+/** @internal **/
+interface CommonArgs {
+  method: KamereonMethod;
+  accountId: Optional<string>;
+  vin: Optional<string>;
+}
+
+/** @internal **/
+export type ReadApiUrl = keyof Omit<typeof KamereonApi, 'KEY' | PrefixWith<'PERFORM_'>>
+
+/** @internal **/
+export type PerformApiUrl = keyof Pick<typeof KamereonApi, Extract<keyof typeof KamereonApi, PrefixWith<'PERFORM_'>>>
+
+/** @internal **/
+export type KamereonMethod = keyof MethodsOf<NgxKamereonClient> & string;
